72bf4e2454dcde8147c8e8aecaf714667a277248
Joe Bamford <bam.joe99@gmail.com>
2022-11-04 15:15:27 +0000

    Add files via upload

--- pychord/analyzer.py
+++ pychord/analyzer.py
@@ -23,8 +23,6 @@ def find_chords_from_notes(notes: List[str], slash: str) -> List[Chord]:
     for rotated_notes in get_all_rotated_notes(notes):
         rotated_root = rotated_notes[0]
         root_and_positions.append((rotated_root, notes_to_positions(rotated_notes, rotated_notes[0])))
-    # print(root_and_positions)
-    # rap[key] = root_and_positions
     chords = []
     for temp_root, positions in root_and_positions:
         quality = QualityManager().find_quality_from_components(temp_root, positions)
@@ -74,22 +72,9 @@ def get_all_rotated_notes(notes: List[str]) -> List[List[str]]:
     notes_list = list(itertools.permutations(notes))
     return notes_list
 
-def get_modded_raps(rap, key):
-    raps = rap[key]
-    for root, pos in raps:
-        pos = np.array(pos)
-        modpos = (pos[1:]-pos[1]).tolist()
-        print([root, modpos])
-
-# [[0, 8, 11, 15, 17],
-# [0, 8, 11, 17, 27],
-# [0, 8, 15, 23, 29],
-# [0, 8, 15, 17, 23],
-# [0, 8, 17, 23, 27],
-# [0, 8, 17, 27, 35],
-# [0, 11, 20, 27, 29],
-# [0, 11, 20, 29, 39],
-# [0, 11, 15, 20, 29],
-# [0, 11, 15, 17, 20],
-# [0, 11, 17, 20, 27],
-# [0, 11, 17, 27, 32]]
+# def get_modded_raps(rap, key):
+#     raps = rap[key]
+#     for root, pos in raps:
+#         pos = np.array(pos)
+#         modpos = (pos[1:]-pos[1]).tolist()
+#         print([root, modpos])
